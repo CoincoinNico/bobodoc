0110db3bae604f30f1d9c7a4b7cbb77b9f742ba2
Nicolas T <ntournaud@gmail.com>
2014-05-30 10:04:14 +0200

    Design fixes

--- app/models/drugstore.rb
+++ app/models/drugstore.rb
@@ -2,6 +2,7 @@ class Drugstore < ActiveRecord::Base
   has_many :stocks
   has_many :comments, :as => :commentable
   has_many :medicaments, :through => :stocks
+  # , :select => "medicaments.*, stock.stock_price AS stock_price"
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
   # attr_accessible :stock_list
