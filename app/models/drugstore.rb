d0a7a3026fdd380e76ac8c325be904af551f2111
Nicolas T <ntournaud@gmail.com>
2014-06-05 17:45:44 +0200

    No H10

--- app/models/drugstore.rb
+++ app/models/drugstore.rb
@@ -2,6 +2,8 @@ class Drugstore < ActiveRecord::Base
   has_many :stocks
   has_many :drugstore_comments
   has_many :medicaments, :through => :stocks
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   # , :select => "medicaments.*, stock.stock_price AS stock_price"
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
--- app/models/medicament.rb
+++ app/models/medicament.rb
@@ -2,6 +2,8 @@ class Medicament < ActiveRecord::Base
   has_many :stocks
   has_many :medicament_comments
   has_many :drugstores, :through => :stocks
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   # accepts_nested_attributes_for :stocks, :reject_if => :all_blank, :allow_destroy => true
   def self.search(search)
     where("name ILIKE ?","%#{search}%")
